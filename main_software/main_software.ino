26bd8047eff12b9090429d9a94dd0611ffe60b34
ulrius <ulrik.utheim.sverdrup@gmail.com>
2017-08-11 08:26:17 +0200

    First commit

---
+++ main_software/main_software.ino
@@ -0,0 +1,146 @@
+// Define colour sensor LED pins
+int ledArray[] = {3,5,6};
+
+// Boolean to know if the balance has been set
+boolean balanceSet = false;
+
+// Place holders for colour detected
+int red = 0;
+int green = 0;
+int blue = 0;
+
+// Floats to hold colour arrays
+float colourArray[] = {0,0,0};
+float whiteArray[] = {0,0,0};
+float blackArray[] = {0,0,0};
+
+float colourMax[] = {0,0,0};
+float colourMin[] = {255,255,255};
+float colourMapped[] = {0,0,0};
+
+// Place holder for average
+int avgRead;
+
+void setup() {
+  // Setup the outputs for the colour sensor
+  pinMode(3,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  // Setup the outputs for the visual colourLED
+  pinMode(9,OUTPUT);    // 
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+ 
+  // Begin serial communication
+  Serial.begin(9600);
+}
+
+void loop() {
+  checkBalance();
+  checkColour();
+  printColour();
+  showColor();
+}
+
+void checkBalance(){
+  // Check if the balance has been set, if not, set it
+  if(balanceSet == false){
+    setBalance();
+  }
+}
+
+void setBalance(){
+  digitalWrite(ledArray[0],LOW);
+  digitalWrite(ledArray[1],LOW);
+  digitalWrite(ledArray[2],LOW);
+  digitalWrite(9,LOW);
+  digitalWrite(10,LOW);
+  digitalWrite(11,LOW);
+  delay(50);
+  digitalWrite(ledArray[0],HIGH);
+  digitalWrite(ledArray[1],HIGH);
+  digitalWrite(ledArray[2],HIGH);
+  digitalWrite(9,HIGH);
+  digitalWrite(10,HIGH);
+  digitalWrite(11,HIGH);
+  // Set white balance
+  delay(5000);                              // Delay for five seconds, this gives us time to get a white sample in front of our sensor
+  // Scan the white sample.
+  // Go through each light, get a reading, set the base reading for each colour red, green, and blue to the white array
+  for(int i=0; i<=2; i++){
+   digitalWrite(ledArray[i],LOW);
+   delay(100);
+   getReading(5);          // Number is the number of scans to take for average, this whole function is redundant, one reading works just as well.
+   whiteArray[i] = avgRead;
+   digitalWrite(ledArray[i],HIGH);
+   delay(100);
+  }
+  // Done scanning white, now it will pulse blue to tell you that it is time for the black (or grey) sample.
+  // Set black balance
+  delay(5000);              // Wait for five seconds so we can position our black sample 
+  // Go ahead and scan, sets the colour values for red, green, and blue when exposed to black
+  for(int i=0; i<=2; i++){
+   digitalWrite(ledArray[i],LOW);
+   delay(100);
+   getReading(5);
+   blackArray[i] = avgRead;
+   //blackArray[i] = analogRead(2);
+   digitalWrite(ledArray[i],HIGH);
+   delay(100);
+  }
+  //set boolean value so we know that balance is set
+  balanceSet = true;
+  delay(5000);     //delay another 5 seconds to let us catch up
+}
+
+void checkColour(){
+  for(int i=0; i<=2; i++){
+    digitalWrite(ledArray[i],LOW);  // Turn on the LED, red, green or blue depending which iteration
+    delay(100);                      // Delay to allow CdS to stabalize (they are slow)
+    getReading(5);                   // Take a reading however many times
+    colourArray[i] = avgRead;        // Set the current colour in the array to the average reading
+    float greyDiff = whiteArray[i] - blackArray[i];                   // The highest possible return minus the lowest returns the area for values in between
+    colourArray[i] = (colourArray[i] - blackArray[i])/(greyDiff)*255; // The reading returned minus the lowest value divided by the possible range multiplied by 
+                                                                      // 255 will give us a value roughly between 0-255 representing the value for the current 
+                                                                      // reflectivity (for the colour it is exposed to) of what is being scanned
+    
+    colourMapped[1] = map(colourArray[1],20,250,255,0);
+    colourMapped[2] = map(colourArray[2],8,250,255,0);
+    colourMapped[3] = map(colourArray[3],6,245,255,0);
+    
+    digitalWrite(ledArray[i],HIGH);   // Turn off the current LED
+    delay(100);
+  }
+}
+
+void getReading(int times){
+  int reading;
+  int tally = 0;
+  // Take the reading however many times was requested and add them up
+  for(int i=0; i<times; i++){
+    reading = analogRead(A7);
+    tally = reading + tally;
+    delay(10);
+  }
+  // Calculate the average and set it
+  avgRead = (tally)/times;
+}
+
+// Prints the colour in the colour array, in the next step, we will send this to processing to see how good the sensor works.
+void printColour(){
+  Serial.print("R = ");
+  Serial.println(int(colourArray[0]));
+  Serial.print("G = ");
+  Serial.println(int(colourArray[1]));
+  Serial.print("B = ");
+  Serial.println(int(colourArray[2]));
+  Serial.println();
+  //delay(2000);
+}
+
+void showColor(){
+  analogWrite(9,int(colourMapped[0]));   // Red
+  analogWrite(10,int(colourMapped[1]));    // Blue
+  analogWrite(11,int(colourMapped[2]));  // Green
+}
+
